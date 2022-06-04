4970ac0cf886cc1147c5e64e7b63b3e865df6985
Pratim Chowdhary <pratimchowdhary@Pratims-MacBook-Pro.local>
2022-06-03 22:27:24 -0400

    final

--- main.py
+++ main.py
@@ -16,7 +16,8 @@ if __name__ == '__main__':
 
     print('Evaluating News Sentiment Model:')
     for stock in STOCKS:
-        evaluate_model(stock, 'tweets')
+        evaluate_model(stock, 'news')
     print('Evaluating Tweets Sentiment Model:')
     for stock in STOCKS:
         evaluate_model(stock, 'tweets')
+        
